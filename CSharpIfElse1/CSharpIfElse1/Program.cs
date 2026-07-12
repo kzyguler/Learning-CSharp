b495bbbda178a81424db22c68f8f40fa30ce5894
Kuzey <kuzeyguler39@gmail.com>
2026-07-12 17:24:12 +0300

    Create first C# if-else project

---
+++ CSharpIfElse1/CSharpIfElse1/Program.cs
@@ -0,0 +1,18 @@
+﻿int level = 90;
+
+    if (level >= 100)
+{
+    Console.WriteLine("S Rank");
+}
+    else if (level >= 70)
+{
+    Console.WriteLine("A Rank");
+}
+    else if (level >= 50)
+{
+    Console.WriteLine(" B Rank");
+}
+    else
+{
+    Console.WriteLine("Your level isn't enough to join");
+}
