cba393b1d09edadc4dee07674ab7447842a67b3d
kuzey <kuzey@DESKTOP-T63UG2C>
2026-07-12 13:55:09 +0300

    Proje dosyası ekle.

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("What is your name");
+    string user = Console.ReadLine();
+Console.WriteLine($"Hello {user}!");
+Console.WriteLine("That's the first day of your coding journey.");
+
+Console.WriteLine("How old are you?");
+string old = Console.ReadLine();
+Console.WriteLine($"I'm {old} years old.");
+
+Console.WriteLine("What is your favourite game?");
+string game = Console.ReadLine();
+Console.WriteLine($"My favourite game is {game}");
+
+Console.WriteLine("What is your dream job");
+string job  = Console.ReadLine();
+Console.WriteLine($"My dream job is {job}.");
+
+Console.WriteLine("Do your best.");
