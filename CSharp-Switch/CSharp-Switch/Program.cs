271a1749407b1989b6602d8c6a7a1dffafb67927
Kuzey <kuzeyguler39@gmail.com>
2026-07-13 12:10:27 +0300

    Complete logical operators, switch statements and string basics

---
+++ CSharp-Switch/CSharp-Switch/Program.cs
@@ -0,0 +1,9 @@
+﻿string playerName = "Kuzey";
+string favouriteGame = "Elden Ring";
+string university = "Ca Foscari";
+
+Console.WriteLine("Player Name:" +  playerName);
+Console.WriteLine("==================");
+Console.WriteLine("Favourite Game:" + favouriteGame);
+Console.WriteLine("==================");
+Console.WriteLine("University:" + university);
